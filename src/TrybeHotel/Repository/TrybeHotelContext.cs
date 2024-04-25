32a06801d361ee0d8d784f3321c6457cea92f2d0
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-04-25 19:10:00 -0300

    Requisito 1: Implemente as models da aplicação/ DONE

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,6 +4,15 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
     public TrybeHotelContext() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 }
