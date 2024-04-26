b7e0b9cf9356307e18c63d63d6fad32ee96f3bfc
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-04-25 21:35:02 -0300

    Requsito 4: Desenvolva o endpoint GET /hotel/ DONE

--- src/TrybeHotel/Controllers/HotelController.cs
+++ src/TrybeHotel/Controllers/HotelController.cs
@@ -19,7 +19,7 @@ namespace TrybeHotel.Controllers
         // 4. Desenvolva o endpoint GET /hotel
         [HttpGet]
         public IActionResult GetHotels(){
-            throw new NotImplementedException();
+            return Ok(_repository.GetHotels());
         }
 
         // 5. Desenvolva o endpoint POST /hotel
--- src/TrybeHotel/Repository/HotelRepository.cs
+++ src/TrybeHotel/Repository/HotelRepository.cs
@@ -14,7 +14,14 @@ namespace TrybeHotel.Repository
         // 4. Desenvolva o endpoint GET /hotel
         public IEnumerable<HotelDto> GetHotels()
         {
-            throw new NotImplementedException();
+            return _context.Hotels.Select(h => new HotelDto
+            {
+                hotelId = h.HotelId,
+                name = h.Name,
+                address = h.Address,
+                cityId = h.CityId,
+                cityName = h.City!.Name
+            }).ToList();
         }
         
         // 5. Desenvolva o endpoint POST /hotel
